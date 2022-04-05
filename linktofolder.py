9f3b656f767d4e3b557dccc605ebc3f21641fbff
TrevorW-code <trwa4172@colorado.edu>
2022-04-05 14:03:34 -0600

    fixed a few bugs

--- linktofolder.py
+++ linktofolder.py
@@ -1,10 +1,38 @@
-from pytube import YouTube
-import re
-import sys
+'''
+want to add flags for downloading playlists (pl or p) and individual songs (s)
 
-file = str(sys.argv[1])
-# correct_link = re.sub("music.","",file)
-yt=YouTube(file)
+why does it download double the time?
+
+
+'''
+
+from pytube import YouTube
+import sys
+import re
+
+def timeToMin(raw_sec):
+    seconds = raw_sec % 60
+    min = raw_sec // 60
+    return (str(min)+"min "+str(seconds)+"s")
+
+link = str(sys.argv[1])
+yt=YouTube(link)
 t=yt.streams.filter(only_audio=True)
 path = "/Users/trevor/Desktop/ytmDownloader/test"
-t[1].download(path)
+filename=yt.title+" - "+yt.author[:-8]+".mp3" # check to see if mp3 works
+print(yt.title+" - "+yt.author[:-8])
+print("length: "+timeToMin(yt.length))
+
+print('download to: '+path)
+t[1].download(path,filename=filename)
+
+path_to_download = path+"/"+re.sub(" ","\\ ",filename)
+print(path_to_download)
+
+# .order_by('resolution')
+# scoped in the right place?
+
+
+
+
+
